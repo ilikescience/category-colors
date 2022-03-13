54be27287fbf7a16676be2ca209ff86b0be1ed16
Matthew Ström <matthew.h.strom@gmail.com>
2022-03-13 13:06:52 -0400

    simulated annealing is working

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
 const chroma = require("chroma-js");
 
+const stripeColors = [
+    "#f5fbff",
+    "#d6ecff",
+    "#a4cdfe",
+    "#7dabf8",
+    "#6c8eef",
+    "#5469d4",
+    "#3d4eac",
+    "#2f3d89",
+    "#212d63",
+    "#131f41",
+    "#efffed",
+    "#cbf4c9",
+    "#85d996",
+    "#33c27f",
+    "#1ea672",
+    "#09825d",
+    "#0e6245",
+    "#0d4b3b",
+    "#0b3733",
+    "#082429",
+    "#fcf9e9",
+    "#f8e5b9",
+    "#efc078",
+    "#e5993e",
+    "#d97917",
+    "#bb5504",
+    "#983705",
+    "#762b0b",
+    "#571f0d",
+    "#3a1607",
+    "#fff8f5",
+    "#fde2dd",
+    "#fbb5b2",
+    "#fa8389",
+    "#ed5f74",
+    "#cd3d64",
+    "#a41c4e",
+    "#80143f",
+    "#5e1039",
+];
+
 // random from array
 const randomFromArray = (array) => {
     return array[Math.floor(Math.random() * array.length)];
@@ -21,6 +63,12 @@ const sortByHue = (colorArray) => {
     });
 };
 
+const getClosestColor = (color, colorArray) => {
+    const distances = colorArray.map((c) => distance(color, c));
+    const minIndex = distances.indexOf(Math.min(...distances));
+    return colorArray[minIndex];
+};
+
 // array of distances between all points in a color array
 const distances = (colorArray) => {
     const distances = [];
@@ -30,7 +78,7 @@ const distances = (colorArray) => {
         }
     }
     return distances;
-}
+};
 
 // get average of interger array
 const average = (array) => array.reduce((a, b) => a + b) / array.length;
@@ -39,7 +87,7 @@ const average = (array) => array.reduce((a, b) => a + b) / array.length;
 const range = (array) => {
     const sorted = array.sort((a, b) => a - b);
     return sorted[sorted.length - 1] - sorted[0];
-}
+};
 
 // produces a color a small random distance away from the given color
 const randomNearbyColor = (color) => {
@@ -54,34 +102,49 @@ const randomNearbyColor = (color) => {
     return color.set(`rgb.${"rgb"[channelToChange]}`, newVal * 255);
 };
 
-const sigmoid = (x) => 1 / (1 + Math.abs(x));
+// average of distances between array of colors and stripe colors
+const averageDistanceFromStripeColors = (colors) => {
+    const distances = colors.map((c) =>
+        distance(c, getClosestColor(c, stripeColors))
+    );
+    return average(distances);
+};
 
-const evaluateColorArray = (colorArray) => {
-    const averageScore = average(distances(colorArray)); // higher average distance is better
-    const rangeScore = range(distances(colorArray)); // lower range is better
-    return {
-        averageScore,
-        rangeScore,
-    };
+const cost = (state) => {
+    const rangeWeight = 1;
+    const averageWeight = 1;
+    const stripeWeight = 2;
+
+    const theseDistances = distances(state);
+
+    const rangeScore = range(theseDistances); // lower is better
+    const averageScore = 100 - average(theseDistances); // higher is better
+
+    const stripeScore = averageDistanceFromStripeColors(state); // lower is better
+
+    return (
+        rangeWeight * rangeScore +
+        averageWeight * averageScore +
+        stripeWeight * stripeScore
+    );
 };
 
 // find n colors that are equidistant from each other
 // using simulated annealing
 const optimize = (n = 5) => {
     // initialize colors
-    const colors = [];
-    for (let i = 0; i < n; i++) {
+    const colors = [chroma('#635bff')];
+    for (let i = 1; i < n; i++) {
         colors.push(randomColor());
     }
 
     // intialize hyperparameters
-    let temperature = 1;
-    const coolingRate = 0.001;
-    const rangeWeight = 1;
-    const averageWeight = 1;
+    let temperature = 1000;
+    const coolingRate = 0.99;
+    const cutoff = 0.0001;
 
     // iteration loop
-    while (temperature > 0) {
+    while (temperature > cutoff) {
         // for each color
         for (let i = 0; i < colors.length; i++) {
             // copy old colors
@@ -89,11 +152,17 @@ const optimize = (n = 5) => {
             // move the current color randomly
             newColors[i] = randomNearbyColor(newColors[i]);
 
-            // todo: figure out how to evaluate the new colors
+            const delta = cost(newColors) - cost(colors);
+            const probability = Math.exp(-delta / temperature);
+            if (Math.random() < probability) {
+                colors[i] = newColors[i];
+            }
         }
 
+        console.log(cost(colors));
+
         // decrease temperature
-        temperature -= coolingRate;
+        temperature *= coolingRate;
     }
 
     console.log(colors.map((color) => color.hex()));
